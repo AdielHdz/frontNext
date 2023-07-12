84d467e3026bc14ab6d4895622d8d36ee0c9eb35
Adiel <wereber.dragons14@live.com.mx>
2023-07-12 09:00:58 -0600

    espacio

--- proofapp/src/app/page.js
+++ proofapp/src/app/page.js
@@ -13,7 +13,8 @@ export default function Home() {
       <div className="w-full flex items-center justify-center">
       <button className={`relative border-2 border-green-800 w-3/5 rounded-full pt-2 hover:bg-green-800 shadow-shadowButtons hover:text-white transition duration-300 pb-2  text-green-800 `}
       onClick={() => alert("Añadido al carrito correctamente")} 
-      >
+      > 
+      
         <h5 className="relative inline ">
           Agregar al carrito
           <AiOutlineShoppingCart className="inline-block text-xl absolute top-0 -right-7 " />
