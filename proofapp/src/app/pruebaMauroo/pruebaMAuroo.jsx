ac8758a5e95ef670d6000b854478fcaa1ea65817
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-07-12 14:20:07 -0300

    Pruba

---
+++ proofapp/src/app/pruebaMauroo/pruebaMAuroo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PruebaMuro = () => {
+  return (
+    <div className="text-white bg-black h-screen">
+      <h1 className="text-xl font-medium p-3 text-center">Mauroo</h1>
+    </div>
+  );
+};
+
+export default PruebaMuro;
